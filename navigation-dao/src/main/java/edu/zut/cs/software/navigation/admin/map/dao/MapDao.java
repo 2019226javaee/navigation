83dbf1d821bc7c1133102f9766b8d9e5d7fc10f1
user.zff <goodgame_glb@atguigu.com>
2019-05-22 18:04:55 +0800

    5221804deleteall

--- navigation-dao/src/main/java/edu/zut/cs/software/navigation/admin/map/dao/MapDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.software.navigation.admin.map.dao;
-
-import edu.zut.cs.software.navigation.admin.map.domain.Map;
-import edu.zut.cs.software.navigation.base.dao.GenericTreeDao;
-
-public interface MapDao extends GenericTreeDao<Map, Long> {
-
-}
