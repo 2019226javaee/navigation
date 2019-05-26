f05bc9809748e1841271163f84dfc22c5f8bf215
abcd <abcd@efgh.com>
2019-05-26 19:20:14 +0800

    526-roadDaoTest

--- navigation-dao/src/test/java/edu/zut/cs/software/navigation/admin/road/RoadDaoTest.java
+++ navigation-dao/src/test/java/edu/zut/cs/software/navigation/admin/road/RoadDaoTest.java
@@ -12,23 +12,20 @@ import edu.zut.cs.software.navigation.admin.road.dao.RoadDao;
 import edu.zut.cs.software.navigation.admin.road.domain.Road;
 import edu.zut.cs.software.navigation.base.dao.GenericDaoTestCase;
 
-@SuppressWarnings("hiding")
-public class RoadDaoTest<RoadDao> extends GenericDaoTestCase<Long, Road, edu.zut.cs.software.navigation.admin.road.dao.RoadDao>{
+public class RoadDaoTest extends GenericDaoTestCase<Long, Road, RoadDao>{
 	
 	@Autowired
 	RoadDao roadDao;
-	private static final Logger logger = LogManager.getLogger(edu.zut.cs.software.navigation.admin.road.dao.RoadDao.class.getName());
+	private static final Logger logger = LogManager.getLogger(RoadDaoTest.class.getName());
 	public void setRoadDao(RoadDao roadDao) {
 		this.roadDao = roadDao;
-		this.dao = (edu.zut.cs.software.navigation.admin.road.dao.RoadDao) this.roadDao;
+		this.dao =  this.roadDao;
 	}
-	@SuppressWarnings("unchecked")
 	@Test
 	public void testFindAll() {
 		List<Road> all = ((JpaRepository<Road, Long>) this.roadDao).findAll();
 		if (logger.isInfoEnabled()) {
-			logger.info("testFindAll() - List<road> all={}", all); 
+			logger.info("testFindAll() - List<road> all={}", all);
 		}
 	}
 }
-
