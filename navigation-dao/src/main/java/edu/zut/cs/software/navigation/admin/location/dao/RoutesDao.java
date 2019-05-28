fe90b77f2f1246b52f2e8fa8a28b5e22a04b8e28
Sakura <935761799@Sakura.com>
2019-05-28 22:16:44 +0800

    yangming's commit

---
+++ navigation-dao/src/main/java/edu/zut/cs/software/navigation/admin/location/dao/RoutesDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.software.navigation.admin.location.dao;
+
+import edu.zut.cs.software.navigation.admin.location.domain.Routes;
+import edu.zut.cs.software.navigation.base.dao.GenericTreeDao;
+
+public interface RoutesDao extends GenericTreeDao<Routes, Long> {
+
+}
---
+++ navigation-dao/src/main/java/edu/zut/cs/software/navigation/admin/location/domain/Locations.java
@@ -0,0 +1,71 @@
+package edu.zut.cs.software.navigation.admin.location.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import edu.zut.cs.software.navigation.admin.map.domain.Map;
+import edu.zut.cs.software.navigation.base.domain.BaseEntity;
+/*
+ * 地点
+ * 地点-地图
+ * Many to one
+ */
+@Table(name= "N_Locations")
+@Entity
+public class Locations extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8223175360331280998L;
+	
+	
+	@Column(name="X")
+	Integer X;
+	
+	@Column(name="Y")
+	Integer Y;
+	@Column(name="name")
+	String name;
+
+	@ManyToOne
+	@JoinColumn(name = "Route_ID")
+	Routes routes;
+
+	public Integer getX() {
+		return X;
+	}
+
+	public void setX(String leftlocation) {
+		this.X = X;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getY() {
+		return Y;
+	}
+
+	public void setY(Integer Y) {
+		this.Y = Y;
+	}
+	
+	public Routes getRoutes() {
+		return routes;
+	}
+
+	public void setRoutes(Routes routes) {
+		this.routes = routes;
+	}
+
+	
+}
