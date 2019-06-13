2608430ee12c50a9399a6970a1be8e0675579be4
abcd <abcd@efgh.com>
2019-06-13 09:55:26 +0800

    613 commit

--- navigation-web/src/main/java/edu/zut/cs/software/navigation/offset/web/spring/controller/OffsetController.java
+++ navigation-web/src/main/java/edu/zut/cs/software/navigation/offset/web/spring/controller/OffsetController.java
@@ -1,5 +1,46 @@
 package edu.zut.cs.software.navigation.offset.web.spring.controller;
 
-public class OffsetController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.zut.cs.software.navigation.admin.offset.domain.Offset;
+import edu.zut.cs.software.navigation.base.web.spring.controller.GenericController;
+import edu.zut.cs.software.navigation.offset.service.OffsetManager;
+
+@Controller
+@RequestMapping("/offset")
+public class OffsetController extends GenericController<Offset, Long,OffsetManager>{
+
+	
+	OffsetManager offsetManager;
+	
+	@Autowired
+    public void setOffsetManager(OffsetManager offsetManager) {
+        this.offsetManager = offsetManager;
+        this.manager = this.offsetManager;
+    }
+	@RequestMapping("/offsetdemo")
+    public String index() {
+        return "success";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "hello", produces = "application/json;charset=utf-8")
+    public String hello() {
+        return "Hello, This is 偏移量!";
+    }
+    
+    @ResponseBody
+	@GetMapping(value = "all",produces = "application/json;charset=utf-8")
+	public List<Offset> findAllOffset(){
+		return this.manager.findAll();
+	}
+
+
+	
 }
--- navigation-web/src/main/java/edu/zut/cs/software/navigation/offset/web/spring/controller/PointController.java
+++ navigation-web/src/main/java/edu/zut/cs/software/navigation/offset/web/spring/controller/PointController.java
@@ -1,5 +1,54 @@
 package edu.zut.cs.software.navigation.offset.web.spring.controller;
 
-public class PointController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageInfo;
+
+import edu.zut.cs.software.navigation.admin.offset.domain.Point;
+import edu.zut.cs.software.navigation.base.web.spring.controller.GenericController;
+import edu.zut.cs.software.navigation.offset.service.PointManager;
+
+@Controller
+@RequestMapping("/point")
+public class PointController extends GenericController<Point, Long, PointManager>{
+
+	
+	PointManager pointManager;
+	
+	@Autowired
+    public void setPlaceManager(PointManager pointManager) {
+        this.pointManager = pointManager;
+        this.manager = this.pointManager;
+    }
+	@RequestMapping("/index")
+    public String index() {
+        return "point/index";
+    }
+
+
+
+    @ResponseBody
+	@RequestMapping(value = "all", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	public String findAllPoint() {
+    	List<Point> pointList = this.manager.findAll();
+        PageInfo pageInfo = new PageInfo(pointList);
+        return JSON.toJSONString(pageInfo);
+	}
+    
+    @ResponseBody
+	@GetMapping(value = "allex",produces = "application/json;charset=utf-8")
+	public List<Point> findAllRouteex(){
+		return this.manager.findAll();
+	}
+
+
+	
 }
