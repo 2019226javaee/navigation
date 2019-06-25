72ce1fed651760932a715617b723383973867f4a
zzff <goodgame_glb@atguigu.com>
2019-06-25 13:41:46 +0800

    625 commit for maptable

--- vue_navigation/src/components/nvgtable/maptable.vue
+++ vue_navigation/src/components/nvgtable/maptable.vue
@@ -3,41 +3,49 @@
     :data="tableData"
     height="650px"
     style="width: 100%">
+
     <el-table-column
-      label="日期"
-      width="200">
+      label="创建日期"
+      width="300">
       <template slot-scope="scope">
         <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ getLocalTime(scope.row.creatTime) }}</span>
+        <span style="margin-left: 10px">{{date}}</span>
+      </template>
+    </el-table-column>
+    <el-table-column  label="ID" width="200">
+      <template slot-scope="scope">
+        <span>{{ scope.row.id}}</span>
       </template>
     </el-table-column>
 
     <el-table-column
-      label="货品"
-      width="180">
+      label="地图名称"
+      width="250">
       <template slot-scope="scope">
         <!--<i class="el-icon-time"></i>-->
-        <span style="margin-left: 10px">{{ scope.row.goodName }}</span>
+        <span style="margin-left: 10px">{{ scope.row.name }}</span>
       </template>
     </el-table-column>
 
+
+
     <el-table-column
-      label="负责人"
-      width="160">
+      label="地图面积"
+      width="200">
       <template slot-scope="scope">
         <!--<i class="el-icon-time"></i>-->
-        <span style="margin-left: 10px">{{ scope.row.dealByPersonName }}</span>
+        <span style="margin-left: 10px">{{ scope.row.area }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="地点个数"
+      width="200">
+      <template slot-scope="scope">
+        <!--<i class="el-icon-time"></i>-->
+        <span style="margin-left: 10px">{{ scope.row.placenum }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column
-      label="重量"
-      width="180">
-      <template slot-scope="scope">
-        <!--<i class="el-icon-time"></i>-->
-        <span style="margin-left: 10px">{{ scope.row.dealMoney }}</span>
-      </template>
-    </el-table-column>
     <el-table-column label="操作">
       <template slot-scope="scope">
         <!--<el-button-->
@@ -47,16 +55,17 @@
         <!-- Form -->
         <el-button type="primary" @click="dialogFormVisible = true" size="mini">新增</el-button>
 
-        <el-dialog title="采购" :visible.sync="dialogFormVisible">
-          <el-form :model="epdtForm">
-            <el-form-item label="货品名称" :label-width="formLabelWidth">
-              <el-input v-model="epdtForm.goodName" autocomplete="off"></el-input>
+        <el-dialog title="地图增加" :visible.sync="dialogFormVisible">
+          <el-form :model="Form">
+            <el-form-item label="地图名称" :label-width="formLabelWidth">
+              <el-input v-model="Form.name" autocomplete="off"></el-input>
             </el-form-item>
-            <el-form-item label="负责人" :label-width="formLabelWidth">
-              <el-input v-model="epdtForm.dealByPersonName" autocomplete="off"></el-input>
+            <el-form-item label="地图面积" :label-width="formLabelWidth">
+              <el-input v-model="Form.area" autocomplete="off">
+              </el-input>
             </el-form-item>
-            <el-form-item label="重量" :label-width="formLabelWidth">
-              <el-input v-model="epdtForm.dealMoney" autocomplete="off">
+            <el-form-item label="地点个数" :label-width="formLabelWidth">
+              <el-input v-model="Form.placenum" autocomplete="off">
               </el-input>
             </el-form-item>
           </el-form>
@@ -77,7 +86,7 @@
         <el-button
           type="success"
           size="mini"
-          @click="updateExpenditure(scope.$index, scope.row)">编辑</el-button>
+          @click="update(scope.$index, scope.row)">修改</el-button>
 
 
       </template>
@@ -92,98 +101,49 @@
         tableData: [],
         dialogTableVisible: false,
         dialogFormVisible: false,
-        epdtForm: {
-          goodName: '',
-          dealByPersonName: '',
-          dealMoney: ''
+        Form: {
+          name: '',
+          area: '',
+          id: '',
+          placenum: '',
+          date:'',
         },
         formLabelWidth: '120px',
-        // epdtFormToUpdate:{
-        //
-        // }
+        date:new Date(),
       }
     },
     methods: {
-      updateExpenditure(index,row) {
-        this.$prompt('请输入更改的重量', '修改', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-        }).then(({value}) => {
-          this.$message({
-            type: 'success',
-            message: '已保存: '
-          });
-          console.log(row.id, value);
-
-
-          this.$axios({
-            method: "put",
-            url: this.HOST + '/sun/expenditure/info?id=' + row.id + "&dealMoney=" + value,
-            data:{
-
-            },
-          })
-            .then(function (response) {
-
-              console.log(response);
-
-            })
-
-            .catch(function (error) {
-
-              console.log(error);
-
-            });
-
-
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '取消输入'
-          });
-        });
+      update(index,row) {
       },
-
       postForm() {
-        const url = this.HOST + '/navigation-web/map/all';
+        const url = this.HOST + '/navigation-web/map/save';
         this.dialogFormVisible = false;
-
         var params = new URLSearchParams();
-        params.append('goodName', this.epdtForm.goodName);
-        params.append('dealByPersonName', this.epdtForm.dealByPersonName);
-        params.append('dealMoney', this.epdtForm.dealMoney);
-
+        params.append('name', this.Form.name);
+        params.append('area', this.Form.area);
+        params.append('placenum', this.Form.placenum);
         console.log(params);
         this.$axios({
           method: 'post',
           url: url,
           data: params
         })
-
           .then(function (response) {
-
             console.log(response);
-
           })
-
           .catch(function (error) {
-
             console.log(error);
-
           });
       },
-
       handleEdit(index, row) {
         console.log(index, row);
-
       },
-
       handleDelete(index, row) {
         console.log(index, row);
-        var expenditureId = row.id;
-        console.log(expenditureId);
+        var placeId = row.id;
+        console.log(placeId);
         this.$axios
-          .delete(this.HOST + '/navigation-web/map/all' + expenditureId)
+          .delete(this.HOST + '/navigation-web/map/delete/' + placeId)
           .then(res => {
             console.log(res);
             this.tableData.splice(index, 1)
@@ -192,35 +152,35 @@
             console.log(err);
           });
       },
-
       //时间戳转化
       getLocalTime(nS) {
         return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+      },
+      timeNow () {
+        return moment().utc().format('YYYY年MM月DD日') + ' ' + moment().utc().format('dddd')
       }
     },
     created() {
-      this.$axios.get(this.HOST + '/navigation-web/map/all')
-
-      //then获取成功；response成功后的返回值（对象）
-
-        .then(response => {
-
+      this.$axios.get(this.HOST+'/navigation-web/map/all')
+        .then(response=>{
           console.log(response);
-
-          this.tableData = response.data;
-
+          this.tableData=response.data;
         })
-
-        //获取失败
-
-        .catch(error => {
-
+        .catch(error=>{
           console.log(error);
-
           alert('网络错误，不能访问');
-
         })
-
-    }
+    },
+    mounted() {
+      let _this = this;
+      this.timer = setInterval(function() {
+        _this.date = new Date().toLocaleString();
+      });
+    },
+    beforeDestroy: function() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+    },
   }
 </script>
