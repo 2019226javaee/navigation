cea3d25f89fd8c57a6da08afa0a391ced4ceb93c
ys <576925583@qq.com>
2019-06-25 14:19:46 +0800

    625 1419

---
+++ vue_navigation/src/components/nvgtable/keepertable.vue
@@ -0,0 +1,212 @@
+<template>
+  <el-table
+    :data="tableData"
+    height="650px"
+    style="width: 100%">
+    <el-table-column
+      label="日期"
+      width="200">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ getLocalTime(scope.row.creatTime) }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="管理员"
+      width="180">
+      <template slot-scope="scope">
+        <!--<i class="el-icon-time"></i>-->
+        <span style="margin-left: 10px">{{ scope.row.name }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="权限"
+      width="160">
+      <template slot-scope="scope">
+        <!--<i class="el-icon-time"></i>-->
+        <span style="margin-left: 10px">{{ scope.row.access }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <!--<el-button-->
+        <!--size="mini"-->
+        <!--@click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
+
+        <!-- Form -->
+        <el-button type="primary" @click="dialogFormVisible = true" size="mini">新增</el-button>
+
+        <el-dialog title="获取权限" :visible.sync="dialogFormVisible">
+          <el-form :model="epdtForm">
+            <el-form-item label="管理员" :label-width="formLabelWidth">
+              <el-input v-model="epdtForm.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="权限" :label-width="formLabelWidth">
+              <el-input v-model="epdtForm.access" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="postForm">确 定</el-button>
+          </div>
+        </el-dialog>
+
+
+
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+
+
+        <el-button
+          type="success"
+          size="mini"
+          @click="updateExpenditure(scope.$index, scope.row)">编辑</el-button>
+
+
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [],
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        epdtForm: {
+          name: '',
+          access: '',
+        },
+        formLabelWidth: '120px',
+        // epdtFormToUpdate:{
+        //
+        // }
+      }
+    },
+    methods: {
+      updateExpenditure(index,row) {
+        this.$prompt('请输入更改的权限', '修改', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+        }).then(({value}) => {
+          this.$message({
+            type: 'success',
+            message: '已保存: '
+          });
+          console.log(row.id, value);
+
+
+          this.$axios({
+            method: "put",
+            url: this.HOST + '/sun/expenditure/info?id=' + row.id + "&dealMoney=" + value,
+            data:{
+
+            },
+          })
+            .then(function (response) {
+
+              console.log(response);
+
+            })
+
+            .catch(function (error) {
+
+              console.log(error);
+
+            });
+
+
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消输入'
+          });
+        });
+      },
+
+      postForm() {
+        const url = this.HOST + '/navigation-web/keeper/all';
+        this.dialogFormVisible = false;
+
+        var params = new URLSearchParams();
+        params.append('name', this.epdtForm.name);
+        params.append('access', this.epdtForm.access);
+
+        console.log(params);
+        this.$axios({
+          method: 'post',
+          url: url,
+          data: params
+        })
+
+          .then(function (response) {
+
+            console.log(response);
+
+          })
+
+          .catch(function (error) {
+
+            console.log(error);
+
+          });
+      },
+
+      handleEdit(index, row) {
+        console.log(index, row);
+
+      },
+
+      handleDelete(index, row) {
+        console.log(index, row);
+        var expenditureId = row.id;
+        console.log(expenditureId);
+        this.$axios
+          .delete(this.HOST + '/navigation-web/keeper/all' + expenditureId)
+          .then(res => {
+            console.log(res);
+            this.tableData.splice(index, 1)
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+
+      //时间戳转化
+      getLocalTime(nS) {
+        return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+      }
+    },
+    created() {
+      this.$axios.get(this.HOST + '/navigation-web/keeper/all')
+
+      //then获取成功；response成功后的返回值（对象）
+
+        .then(response => {
+
+          console.log(response);
+
+          this.tableData = response.data;
+
+        })
+
+        //获取失败
+
+        .catch(error => {
+
+          console.log(error);
+
+          alert('网络错误，不能访问');
+
+        })
+
+    }
+  }
+</script>
